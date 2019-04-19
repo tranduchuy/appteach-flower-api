5ec54e3e9b5ad201339a77b4104b9e35184cb698
Sơn Hoàng <nhsh1997@gmail.com>
2019-04-19 15:00:34 +0700

    add constants

---
+++ src/constant/generals.ts
@@ -0,0 +1,17 @@
+export namespace General {
+  export const Genders = {
+    GENDER_MALE : 1,
+    GENDER_FEMALE : 2
+  };
+
+  export const UserTypes = {
+    TYPE_CUSTOMER : 1,
+    TYPE_SELLER : 2
+  };
+  export const UserRoles = {
+    USER_ROLE_MASTER : 1,
+    USER_ROLE_ADMIN : 2,
+    USER_ROLE_ENDUSER : 3
+  };
+
+}
