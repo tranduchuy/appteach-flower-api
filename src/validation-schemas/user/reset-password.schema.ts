04cc59dc435eaf1d65333febf390f21a48722151
Sơn Hoàng <nhsh1997@gmail.com>
2019-04-27 19:11:54 +0700

    update

---
+++ src/validation-schemas/user/reset-password.schema.ts
@@ -0,0 +1,10 @@
+import Joi from '@hapi/joi';
+
+const ResetPasswordValidationSchema = Joi.object().keys({
+      token: Joi.string().required().max(50),
+      password: Joi.string().required().min(6).regex(/^[a-zA-Z0-9]*$/),
+      confirmedPassword: Joi.string().required().min(6).regex(/^[a-zA-Z0-9]*$/)
+    }
+);
+
+export default ResetPasswordValidationSchema;
