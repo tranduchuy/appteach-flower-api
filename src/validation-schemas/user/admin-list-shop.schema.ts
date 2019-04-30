bb8f2cc8e5d74be0d1a60542a922c606d3b2369a
Long Nguyen <nguyenbinhlong92@gmail.com>
2019-04-30 09:54:04 +0700

    Admin list Shop

--- src/validation-schemas/user/admin-list-shop.schema.ts
+++ src/validation-schemas/user/admin-list-shop.schema.ts
@@ -2,8 +2,7 @@ import Joi from '@hapi/joi';
 
 const ListProductSchema = Joi.object().keys(
   {
-    shop_id: Joi.string(),
-    product_name: Joi.string(),
+    name: Joi.string(),
     limit: Joi.number().integer().min(5).max(200),
     page: Joi.number().integer().min(1),
     status: Joi.number(),
