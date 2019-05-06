3fb227952ff9a46e7548d56385186b8abf196da8
HuyTD <vincenthuytd@gmail.com>
2019-05-06 15:09:23 +0700

    Fix bug

--- src/services/mailer.service.ts
+++ src/services/mailer.service.ts
@@ -25,7 +25,7 @@ export class MailerService {
       from: 'cskh.hecta@gmail.com',
       to: email,
       subject: 'Flower VN - Xác nhận đăng kí',
-      text: 'http://157.230.248.161:4000/xac-nhan-tai-khoan/' + token
+      text: 'http://157.230.248.161:4000/#/xac-nhan-tai-khoan/' + token
     };
 
     this.transporter.sendMail(mailOptions, function (error, info) {
