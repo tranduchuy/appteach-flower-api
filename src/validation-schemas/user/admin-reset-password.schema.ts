32a537f6e0ed3a971a4fbeaf5cb93c8a3f9e7033
Sơn Hoàng <nhsh1997@gmail.com>
2019-06-08 16:29:37 +0700

    update

---
+++ src/validation-schemas/user/admin-reset-password.schema.ts
@@ -0,0 +1,9 @@
+import Joi from '@hapi/joi';
+
+const AdminResetPasswordValidationSchema = Joi.object().keys({
+      userId: Joi.string().required(),
+      password: Joi.string().required().min(6).regex(/^[a-zA-Z0-9]*$/)
+    }
+);
+
+export default AdminResetPasswordValidationSchema;
