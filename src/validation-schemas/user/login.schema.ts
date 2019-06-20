c4cf098c561a279345ff8879255bb5a1fbe19cba
HuyTD <vincenthuytd@gmail.com>
2019-06-20 09:53:44 +0700

    Revert code, allow login admin by username

--- src/validation-schemas/user/login.schema.ts
+++ src/validation-schemas/user/login.schema.ts
@@ -2,6 +2,7 @@ import Joi from '@hapi/joi';
 
 const LoginValidationSchema = Joi.object().keys({
     email: Joi.string().max(100),
+    username: Joi.string().min(6).max(100).regex(/^[a-zA-Z0-9]*$/),
     password: Joi.string().min(6).max(100).regex(/^[a-zA-Z0-9]*$/)
   }
 );
