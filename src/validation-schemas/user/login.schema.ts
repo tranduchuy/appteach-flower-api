ca254113c22bf67aeef2e9c5001a3125deb2340d
Hoang Nguyen <nhsh1997@gmail.com>
2019-07-08 13:58:19 +0700

    update console.err for user controllers

--- src/validation-schemas/user/login.schema.ts
+++ src/validation-schemas/user/login.schema.ts
@@ -1,9 +1,9 @@
 import Joi from '@hapi/joi';
 
 const LoginValidationSchema = Joi.object().keys({
-    email: Joi.string().max(100),
+    email: Joi.string().max(100).required(),
     username: Joi.string().min(6).max(100).regex(/^[a-zA-Z0-9]*$/),
-    password: Joi.string().min(6).max(100).regex(/^[a-zA-Z0-9]*$/)
+    password: Joi.string().min(6).max(100).regex(/^[a-zA-Z0-9]*$/).required()
   }
 );
 
