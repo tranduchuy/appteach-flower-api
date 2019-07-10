1d36a5559c5739039a69b1dc566f3029d1b66673
Hoang Nguyen <nhsh1997@gmail.com>
2019-07-10 08:36:30 +0700

    fix bug convert price to number

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -476,7 +476,7 @@ export class ProductController {
         const salePriceCheck = salePrice || product.saleOff.price;
         const price = originalPrice || product.originalPrice;
         // check sale price vs original price.
-        if (salePriceCheck > price) {
+        if (Number(salePriceCheck) > Number(price)) {
           const result: IRes<{}> = {
             status: HttpStatus.BAD_REQUEST,
             messages: [ResponseMessages.Product.NOT_VALID_PRICE],
