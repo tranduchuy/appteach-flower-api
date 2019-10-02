704384fe6af47ccf98f652e82d01a245ed7f3cfd
HuyTD <vincenthuytd@gmail.com>
2019-10-02 14:22:16 +0700

    Create shop2.model

---
+++ src/models/shop.model.ts
@@ -0,0 +1,68 @@
+import { Model, DataTypes } from 'sequelize';
+import { MYSQL_CONNECTION } from '../utils/secrets';
+import User2 from './user.model';
+
+export class Shop2 extends Model {
+  id!: number;
+  name: string;
+  slug!: string;
+  status: number;
+  availableShipCountry: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  discountRate: number;
+  usersId!: number;
+}
+
+Shop2.init({
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    field: 'ID',
+    primaryKey: true,
+    unique: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING(255),
+    field: 'NAME'
+  },
+  slug: {
+    type: DataTypes.STRING(255),
+    field: 'SLUG',
+    unique: true
+  },
+  status: {
+    type: DataTypes.NUMBER,
+    field: 'STATUS',
+  },
+  availableShipCountry: {
+    type: DataTypes.TINYINT,
+    field: 'AVAILABLE_SHIP_COUNTRY'
+  },
+  createdAt: {
+    field: 'CREATED_AT',
+    type: DataTypes.DATE
+  },
+  updatedAt: {
+    field: 'UPDATED_AT',
+    type: DataTypes.DATE
+  },
+  discountRate: {
+    field: 'DISCOUNT_RATE',
+    type: DataTypes.FLOAT
+  },
+  usersId: {
+    field: 'USERS_ID',
+    type: DataTypes.INTEGER.UNSIGNED
+  }
+}, {
+  tableName: 'SHOPS',
+  freezeTableName: true,
+  sequelize: MYSQL_CONNECTION
+});
+
+Shop2.hasOne(User2, {
+  foreignKey: 'USERS_ID'
+});
+
+export default Shop2;
