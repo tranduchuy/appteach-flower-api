ab9b3adf18b21eb4a5e259291ed5c176e1ea4e80
HuyTD <vincenthuytd@gmail.com>
2019-10-10 08:47:46 +0700

    Demo some test script

--- src/services/user.service.test.ts
+++ src/services/user.service.test.ts
@@ -1,4 +1,5 @@
 import { UserService } from './user.service';
+import UserModel from '../models/user';
 // import { General } from '../constant/generals';
 import bcrypt from 'bcrypt';
 // import UserTypes = General.UserTypes;
@@ -23,7 +24,6 @@ describe('UserService', () => {
       const isValid = userService.isValidHashPassword('asdasd', '123456');
       expect(spy).toHaveBeenCalled();
       expect(isValid).toBe(false);
-      // spy.mockRestore();
     });
 
     it('isValid should be true if bcrypt.compareSync return true', () => {
@@ -31,44 +31,37 @@ describe('UserService', () => {
         .mockReturnValue(true);
 
       const isValid = userService.isValidHashPassword('asdasd', '123456');
-      console.log('=========');
-      console.log(isValid);
 
       expect(spy).toHaveBeenCalledWith('123456', 'asdasd');
       expect(isValid).toBe(true);
-      // spy.mockRestore();
+    });
+  });
+
+  describe('findById', () => {
+
+    const user = {
+      id: 1
+    };
+
+    it('Should return null when id not found', async () => {
+      const spy = jest.spyOn(UserModel, 'findById')
+        .mockReturnValue(null);
+
+      const result = await userService.findById('1');
+      expect(spy).toHaveBeenCalledWith('1');
+      expect(result).toBeNull();
+    });
+
+    it('Should return object of user when id found', async () => {
+      const spy = jest.spyOn(UserModel, 'findById')
+        .mockReturnValue(user);
+
+      const result = await userService.findById('1');
+      expect(spy).toHaveBeenCalledWith('1');
+      expect(result).toEqual(user);
     });
   });
 });
-
-
-//
-// describe('createUser function', () => {
-//     it('newUser should return data of user when user was created successfully', async () => {
-//         const newUserData = {
-//             email: 'tuevo@gmail.com',
-//             name: 'tue vo',
-//             password: 'asdasd',
-//             type: UserTypes.TYPE_CUSTOMER,
-//             role: null,
-//             phone: '093659211',
-//             gender: 1,
-//             city: null,
-//             district: null,
-//             ward: null,
-//             registerBy: RegisterByTypes.NORMAL,
-//             address: 'vietnam',
-//             otpCode: null,
-//           };
-//         const newUser = await userService.createUser(newUserData);
-//
-//         expect(newUser).not.toBeNull();
-//     });
-// });
-
-
-
-
 
 
 
