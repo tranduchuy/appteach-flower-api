f591e99515129a67f3277c9a9fad7e7dec815d30
HuyTD <vincenthuytd@gmail.com>
2019-10-10 11:33:18 +0700

    Create file to include all model before create relation

---
+++ src/models/index.ts
@@ -0,0 +1,9 @@
+import Address2 from './address.model';
+import District from './district.model';
+import User2 from './user.model';
+import City from './city.model';
+
+City.hasMany(User2, {foreignKey: 'id', as: 'users'});
+User2.hasMany(Address2, {foreignKey: 'USERS_ID'});
+User2.belongsTo(City, {foreignKey: 'city', as: 'cityInfo', targetKey: 'id'});
+User2.hasOne(District, {foreignKey: 'ID', as: 'district'});
